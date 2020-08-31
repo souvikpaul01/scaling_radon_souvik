86c6445a24337ae32c20eed2ee79ff8115730c46
souvikpaul01 <souvik.paul01@gmail.com>
2020-08-31 12:39:07 +0000

    Update Commit

--- webApp-loadbalancer-node_exporter-TOSCA/scaleup.py
+++ webApp-loadbalancer-node_exporter-TOSCA/scaleup.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+#need to use the other python file was module library to this file
 file_to_delete = "/home/ubuntu/opera/xopera-opera/examples/nginx_openstack/webApp-loadbalancer-node_exporter-TOSCA/.opera/instances/nginx-lb_0"
 try:
     os.remove(file_to_delete)
